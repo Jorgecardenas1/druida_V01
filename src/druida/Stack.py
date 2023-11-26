707110d23ba07a22d8c719576b49a5462abad7fc
JORGE HERNAN CARDENAS <jorgehernancardenas@JORGEs-MacBook-Pro.local>
2023-11-26 20:27:51 -0300

    autoencoder and latent diffusion first attempt

--- src/druida/Stack.py
+++ src/druida/Stack.py
@@ -132,12 +132,11 @@ class Trainer:
             overall_loss = 0
 
             for batch_idx, (x, _) in enumerate(pbar):
-                
                 images=x.to(self.device)
                 images = (images.clamp(-1, 1) + 1) / 2 #make all values between 0 and 1 
                 #images = (x * 255).type(torch.uint8) #valid pixel range
                 #images=images.reshape(self.batch_size,-1).to(self.device)
-                images = images.view(self.batch_size, 64*64*3).to(self.device)
+                images = images.view(images.size()[0], (64*64*3)).to(self.device)
                 optimizer.zero_grad()
 
                 x_hat, mean, log_var = model(images)
@@ -555,6 +554,7 @@ class Diffusion:
         return x
     
 
+    #Sample conditional diffusion
     def sample_cdm(self, model, n, labels, cfg_scale=3):
         logging.info(f"Sampling {n} new images....")
         model.eval()
@@ -586,3 +586,72 @@ class Diffusion:
 
 
 
+"""Stable difussion stack"""
+class DiffusionWrapper(nn.Module):
+    def __init__(self, diffusion_model: toolkit.UNetModel):
+        super().__init__()
+        self.diffusion_model = diffusion_model
+
+    def forward(self, x: torch.Tensor, time_steps: torch.Tensor, context: torch.Tensor):
+        return self.diffusion_model(x, time_steps, context)
+
+class StableDiffusion(nn.Module):
+    
+    model: DiffusionWrapper
+    first_stage_model: toolkit.Autoencoder
+    cond_stage_model: toolkit.CLIPTextEmbedder
+    
+    def __init__(self,
+                 unet_model: toolkit.UNetModel ,
+                 autoencoder: toolkit.Autoencoder,
+                 clip_embedder: toolkit.CLIPTextEmbedder,
+                 latent_scaling_factor: float,
+                 n_steps: int,
+                 linear_start: float,
+                 linear_end: float,
+                 ):
+        # unet_model is the U-Net that predicts noise ϵ_cond(x_t,c), in latent space
+        # autoencoder is the AutoEncoder
+        # clip_embedder is the CLIP embeddings generator
+        # latent_scaling_factor is the scaling factor for the latent space. The encodings of the autoencoder are scaled by this before feeding into the U-Net.
+        # n_steps is the number of diffusion steps T.
+        # linear_start is the start of the β schedule.
+        # linear_end is the end of the β schedule.
+        super().__init__()
+        self.model = DiffusionWrapper(unet_model)
+
+        self.first_stage_model = autoencoder
+        self.latent_scaling_factor = latent_scaling_factor
+        self.cond_stage_model = clip_embedder 
+
+        self.n_steps = n_steps #Number of steps T
+
+        #ß scheduler
+        beta = torch.linspace(linear_start ** 0.5, linear_end ** 0.5, n_steps, dtype=torch.float64) ** 2
+        self.beta = nn.Parameter(beta.to(torch.float32), requires_grad=False)
+
+        alpha = 1. - beta
+        alpha_bar = torch.cumprod(alpha, dim=0)
+        self.alpha_bar = nn.Parameter(alpha_bar.to(torch.float32), requires_grad=False)
+        
+    @property
+    def device(self):
+        return next(iter(self.model.parameters())).device
+    
+    def get_text_conditioning(self, prompts: list[str]):    
+        
+        return self.cond_stage_model(prompts)
+    
+   #Get scaled latent space representation of the image
+    #The encoder output is a distribution. We sample from that and multiply by the scaling factor.
+    def autoencoder_encode(self, image: torch.Tensor):
+        return self.latent_scaling_factor * self.first_stage_model.encode(image).sample()
+    
+    #Get image from the latent representation
+    #   We scale down by the scaling factor and then decode.
+    def autoencoder_decode(self, z: torch.Tensor):
+        return self.first_stage_model.decode(z / self.latent_scaling_factor)
+    
+    #Predict noise given the latent representation xt, time step t, and the conditioning context c.
+    def forward(self, x: torch.Tensor, t: torch.Tensor, context: torch.Tensor):
+        return self.model(x, t, context)
--- src/druida/tools/utils.py
+++ src/druida/tools/utils.py
@@ -42,9 +42,8 @@ def get_data_denormalize(image_size, dataset_path,batch_size):
 
     transforms = torchvision.transforms.Compose([
         torchvision.transforms.Resize(80),  # args.image_size + 1/4 *args.image_size
-        torchvision.transforms.RandomResizedCrop(image_size, scale=(0.8, 2.0)),
+        torchvision.transforms.RandomResizedCrop(image_size, scale=(0.8, 1.0)),
         torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
 
     ])
 
