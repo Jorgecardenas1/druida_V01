1683dd338934ffcb4f4cf5ddb5722609cd13ca74
JorgeCardenas1 <jorgehernancardenas@gmail.com>
2024-02-01 10:11:53 -0600

    adding softmax and second layer normalization to Vit

--- src/druida/Stack.py
+++ src/druida/Stack.py
@@ -748,3 +748,143 @@ class StableDiffusion(nn.Module):
     #Predict noise given the latent representation xt, time step t, and the conditioning context c.
     def forward(self, x: torch.Tensor, t: torch.Tensor, context: torch.Tensor):
         return self.model(x, t, context)
+
+
+
+"""Vision Transformer"""
+"""PRE LN architecture"""
+
+class AttentionBlock(nn.Module):
+    def __init__(self, embed_dim, hidden_dim, num_heads, dropout=0.0):
+        """Attention Block.
+
+        Args:
+            embed_dim: Dimensionality of input and attention feature vectors
+            hidden_dim: Dimensionality of hidden layer in feed-forward network
+                         (usually 2-4x larger than embed_dim)
+            num_heads: Number of heads to use in the Multi-Head Attention block
+            dropout: Amount of dropout to apply in the feed-forward network
+        """
+        super().__init__()
+
+        self.layer_norm_1 = nn.LayerNorm(embed_dim)
+        self.attn = nn.MultiheadAttention(embed_dim, num_heads)
+        self.layer_norm_2 = nn.LayerNorm(embed_dim)
+        self.linear = nn.Sequential(
+            nn.Linear(embed_dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, embed_dim),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        inp_x = self.layer_norm_1(x)
+        
+        x = x + self.attn(inp_x, inp_x, inp_x)[0]
+        
+        x = self.layer_norm_2(x)
+
+        x = x + self.linear(self.layer_norm_2(x))
+        return x
+    
+class VisionTransformer(nn.Module):
+    def __init__(
+        self,
+       **kwargs
+    ):
+
+        """Vision Transformer.
+
+        Args:
+            embed_dim: Dimensionality of the input feature vectors to the Transformer
+            hidden_dim: Dimensionality of the hidden layer in the feed-forward networks
+                         within the Transformer usually 2-4x larger than embed_dim
+            num_channels: Number of channels of the input (3 for RGB)
+            num_heads: Number of heads to use in the Multi-Head Attention block
+            num_layers: Number of layers to use in the Transformer
+            num_classes: Number of classes to predict
+            patch_size: Number of pixels that the patches have per dimension
+            num_patches: Maximum number of patches an image can have
+            dropout: Amount of dropout to apply in the feed-forward network and
+                      on the input encoding
+        """
+        super().__init__()
+        
+        
+        batch_size=kwargs["batch_size"]
+        embed_dim=kwargs["embed_dim"]
+        hidden_dim=kwargs["hidden_dim"]
+        num_channels=kwargs["num_channels"]
+        num_heads=kwargs["num_heads"]
+        num_layers=kwargs["num_layers"]
+        num_classes=kwargs["num_classes"]
+        patch_size=kwargs["patch_size"]
+        num_patches=kwargs["num_patches"]
+        dropout=kwargs["dropout"]
+
+        self.patch_size = patch_size
+
+        # Layers/Networks
+        self.input_layer = nn.Linear(num_channels * (patch_size**2), embed_dim)
+        self.transformer = nn.Sequential(
+            *(AttentionBlock(embed_dim, hidden_dim, num_heads, dropout=dropout) for _ in range(num_layers))
+        )
+        self.mlp_head = nn.Sequential(nn.LayerNorm(embed_dim), nn.Linear(embed_dim, num_classes))
+        self.dropout = nn.Dropout(dropout)
+
+        # Parameters/Embeddings
+        #self.cls_token = nn.Parameter(torch.randn(1, 1, embed_dim))
+        
+        self.cls_token = nn.Parameter(torch.randn(batch_size, 1, num_channels * patch_size**2))
+        
+        
+        self.pos_embedding = nn.Parameter(torch.randn(batch_size, 1 + num_patches, embed_dim))
+
+        
+        
+    def forward(self, x):
+        # Preprocess input
+        #x = img_to_patch(x, self.patch_size)
+        B, T, _, _ = x.shape
+        x = self.patches(x, patch_size=self.patch_size, flatten_channels=True)
+        x = self.input_layer(x)
+
+        # Add CLS token and positional encoding
+        cls_token = self.cls_token #.repeat(B, 1, 1)
+        #print("CLS Token  ",cls_token.shape)
+        x = torch.cat([cls_token, x], dim=1)
+
+        pos_embed=self.pos_embedding
+        #print("Positional Embedd ",pos_embed.shape)
+        
+        x = x + pos_embed
+        #print("x ", x.shape)
+        # Apply Transforrmer
+        #x = self.dropout(x)
+        #x = x.transpose(0, 1)
+        x = self.transformer(x)
+
+        
+            # Perform classification prediction
+        cls = x[0] ##Class token - supervision signal
+        out = self.mlp_head(cls)
+        return F.softmax(out,dim=1)
+    
+    def patches(self,x, patch_size, flatten_channels):
+        """
+        Args:
+            x: Tensor representing the image of shape [B, C, H, W]
+            patch_size: Number of pixels per dimension of the patches (integer)
+            flatten_channels: If True, the patches will be returned in a flattened format
+                            as a feature vector instead of a image grid.
+        """
+            
+        B, C, H, W = x.shape
+        x = x.reshape(B, C, H // patch_size, patch_size, W // patch_size, patch_size)
+        x = x.permute(0, 2, 4, 1, 3, 5)  # [B, H', W', C, p_H, p_W]
+        x = x.flatten(1, 2)  # [B, H'*W', C, p_H, p_W]
+        if flatten_channels:
+            x = x.flatten(2, 4)  # [B, H'*W', C*p_H*p_W]
+            """Batches, number of patches per image, each flattened patch size including  channel"""
+        return x
